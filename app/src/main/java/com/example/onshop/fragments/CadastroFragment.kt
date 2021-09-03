46fbd3b7a88034dfcb291e2667d042bf3befa578
HenriqueIsrael <henriqueisraeloliveira74@gmail.com>
2021-09-03 17:34:30 -0300

    Implementação firebase.

--- app/src/main/java/com/example/onshop/fragments/CadastroFragment.kt
+++ app/src/main/java/com/example/onshop/fragments/CadastroFragment.kt
@@ -1,12 +1,20 @@
 package com.example.onshop.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.onshop.databinding.CadastroFragmentBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 
 class CadastroFragment : Fragment() {
@@ -30,6 +38,25 @@ class CadastroFragment : Fragment() {
             findNavController().navigateUp()
         }
 
+        binding.btCadastrar.setOnClickListener {
+            if (binding.campoEmailCadastrar.text.toString()
+                    .isEmpty() || binding.campoSenhaCadastrar.text.toString()
+                    .isEmpty() || binding.campoConfirmarSenhaCadastrar.text.toString() != binding.campoSenhaCadastrar.text.toString()
+            ) {
+                Toast.makeText(requireContext(), "Preencha os campos corretamente!", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                val email: String = binding.campoEmailCadastrar.text.toString()
+                val senha: String = binding.campoSenhaCadastrar.text.toString()
+
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,senha)
+                    .addOnCompleteListener{
+                        if(!it.isSuccessful){
+                            Log.d("cadastro","funcionou ${it.result!!.user!!.uid}")
+                        }
+                    }
+            }
+        }
     }
 
 
